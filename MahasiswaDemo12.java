6e96a05dd504d042ad3b12d5c85405691c83712a
Moch.Agung Bima Ardian <bimaagungbimzz@gmail.com>
2026-03-19 10:48:59 +0700

    update1

---
+++ MahasiswaDemo12.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MahasiswaDemo12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Mahasiswa12[] arrayofMahasiswa12 = new Mahasiswa12[3];
+        String dummy;
+
+        for (int i=0; i<3; i++) {
+            arrayofMahasiswa12[i] = new Mahasiswa12();
+
+            System.out.println("Masukkan data mahasiswa ke-" + (i+1));
+            System.out.print("NIM: ");
+            arrayofMahasiswa12[i].nim = sc.nextLine();
+            System.out.print("Nama: ");
+            arrayofMahasiswa12[i].nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            arrayofMahasiswa12[i].kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            arrayofMahasiswa12[i].ipk = sc.nextFloat();
+            sc.nextLine(); 
+            System.out.println("-----------------------------"); 
+        }
+        
+        for (int i=0; i<3; i++) {
+            System.out.println("Data mahasiswa ke-" + (i+1));
+            arrayofMahasiswa12[i].cetakInfo();
+        }
+    }
+}
